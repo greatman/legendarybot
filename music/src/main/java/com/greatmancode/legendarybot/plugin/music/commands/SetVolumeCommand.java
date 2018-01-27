4f961f23e60847469ab443e1c5325e5b165a1329
William Lebel <lebelwilliam@gmail.com>
2018-01-27 18:31:53 -0500

    Add the help.

--- music/src/main/java/com/greatmancode/legendarybot/plugin/music/commands/SetVolumeCommand.java
+++ music/src/main/java/com/greatmancode/legendarybot/plugin/music/commands/SetVolumeCommand.java
@@ -4,6 +4,7 @@ import com.greatmancode.legendarybot.api.LegendaryBot;
 import com.greatmancode.legendarybot.api.commands.AdminCommand;
 import com.greatmancode.legendarybot.plugin.music.MusicPlugin;
 import com.greatmancode.legendarybot.plugin.music.music.GuildMusicManager;
+import net.dv8tion.jda.core.entities.Guild;
 import net.dv8tion.jda.core.entities.Member;
 import net.dv8tion.jda.core.events.message.MessageReceivedEvent;
 
@@ -28,7 +29,6 @@ public class SetVolumeCommand extends AdminCommand {
 
             GuildMusicManager manager = plugin.getMusicManager().getGuildsMusicManager().get(event.getGuild().getId());
             if (manager != null) {
-                System.out.println("WE SET THE VOLUME");
                 manager.player.setVolume(volume);
             }
             plugin.getBot().getGuildSettings(event.getGuild()).setSetting("MUSIC_VOLUME", volume + "");
@@ -51,5 +51,14 @@ public class SetVolumeCommand extends AdminCommand {
     @Override
     public int maxArgs() {
         return 1;
+    }
+
+    @Override
+    public String help(Guild guild) {
+        return "Set the volume of the bot. Number between 1 and 100.";
+    }
+
+    public String shortDescription(Guild guild) {
+        return "Set the volume of the bot. Number between 1 and 100.";
     }
 }
